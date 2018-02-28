abe77c8fddfe54f663cd19aaa08a8f7ba819a146
vadim-and-ilya <vadim.simplex58@yandex.ru>
2018-02-28 12:40:34 +0300

    update LoggerTest iteration01 добавлен метод print()

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -4,32 +4,35 @@ public class Logger {
 
     // char
     public static void log(char message) {
-        System.out.println("char: " + message);
+        print("char: " + message);
     }
 
     // int
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        print("primitive: " + message);
     }
 
     // byte
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        print("primitive: " + message);
     }
 
     // String
     public static void log(String message) {
-        System.out.println("string: " + message);
+        print("string: " + message);
     }
 
     // boolean
     public static void log(boolean message) {
-        System.out.println("primitive: " + message);
+        print("primitive: " + message);
     }
 
     // Object
     public static void log(Object message) {
-        System.out.println("reference: " + message);
+        print("reference: " + message);
     }
 
+    public static void print(String message) {
+        System.out.println(message);
+    }
 }
