01e8f6514f42698c332863428b01c3bdcfef9d20
F1into <vadim.palits@gmail.com>
2018-03-13 11:19:56 +0300

    Annotation @NotNull for Logger.log() function arguments.

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,6 +1,7 @@
 package com.acme.edu;
 
 import com.acme.edu.message.*;
+import com.sun.istack.internal.NotNull;
 
 public class Logger {
 
@@ -17,54 +18,54 @@ public class Logger {
                                     VARARG = "vararg: ";
 
     // PRIMITIVE
-    public static void log(char message) {
+    public static void log(@NotNull char message) {
         controller.direct(new CharMessage(message),CHAR);
     }
 
-    public static void log(int message) {
+    public static void log(@NotNull int message) {
         controller.direct(new IntegerMessage(message), PRIMITIVE);
     }
 
-    public static void log(byte message) {
+    public static void log(@NotNull byte message) {
         controller.direct(new ByteMessage(message), PRIMITIVE);
     }
 
-    public static void log(boolean message) {
+    public static void log(@NotNull boolean message) {
         controller.direct(new BooleanMessage(message), PRIMITIVE);
     }
 
     // STRING
-    public static void log(String message) {
+    public static void log(@NotNull String message) {
         controller.direct(new StringMessage(message), STRING);
     }
 
     // REFERENCE
-    public static void log(Object message) {
+    public static void log(@NotNull Object message) {
         controller.direct(new ObjectMessage(message),REFERENCE);
     }
 
     // Array
-    public static void log(int[] message) {
+    public static void log(@NotNull int[] message) {
         controller.direct(new IntArrayMessage(message), ARRAY);
     }
 
     // Matrix
-    public static void log(int[][] message) {
+    public static void log(@NotNull int[][] message) {
         controller.direct(new IntMatrixMessage(message), MATRIX);
     }
 
     // Multi_Matrix
-    public static void log(int[][][][] message) {
+    public static void log(@NotNull int[][][][] message) {
         controller.direct(new IntMultimatrixMessage(message), MULTI_MATRIX);
     }
 
     // String Array
-    public static void log(String[] message) {
+    public static void log(@NotNull String[] message) {
         controller.direct(new StringArrayMessage(message), STRING_ARRAY);
     }
 
     // Integer Vararg
-    public static void log(int param, int... message) {
+    public static void log(@NotNull int param, int... message) {
         controller.direct(new VarargMessage(param, message), VARARG);
     }
 
